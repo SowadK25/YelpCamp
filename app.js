9c79eeb675233bd22a769862b9dbb058c147e5a0
Sowad <sowadkhan912@gmail.com>
2023-09-20 19:57:08 -0400

    fix

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 
-// Routes
+// Routes 
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
