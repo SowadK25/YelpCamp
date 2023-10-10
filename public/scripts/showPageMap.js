d80382ba6104df78359e62aec89240e5ecb21f1c
Sowad <sowadkhan912@gmail.com>
2023-10-09 21:40:42 -0400

    Fixing styles across app

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -1,17 +1,18 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: coordsArr, // starting position [lng, lat]
-    zoom: 10, // starting zoom
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  center: coordsArr, // starting position [lng, lat]
+  zoom: 10, // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
-    .setLngLat(coordsArr)
-    .setPopup(
-        new mapboxgl.Popup({offset: 25})
-        .setHTML(
-            `<h3>${campTitle}</h3><p>${campLoc}</p>`
-        )
+  .setLngLat(coordsArr)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${campTitle}</h3><p>${campLoc}</p>`
     )
-    .addTo(map)
+  )
+  .addTo(map);
